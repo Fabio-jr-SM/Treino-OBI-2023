2049d525fd1f49afe49350854a4e93ff35d283ea
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-05-26 14:52:53 -0400

    Create rei.c

---
+++ rei.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+A primeira linha da entrada contém dois inteiros N e M, respectivamente, o 
+número de descendentes e o número de participantes da festa. A segunda linha 
+contém N números, representando os pais ou mães dos N descendentes, em ordem 
+crescente: o primeiro número indica o pai ou a mãe do descendente de número 1, 
+o segundo número indica o pai ou a mãe do descendente de número 2, e assim por 
+diante. A terceira linha contém M números, identificando todos os descendentes 
+que compareceram à festa.
+
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int N, M;
+    
+    scanf("%d%d",&N,&M);
+
+    return 0;
+}
